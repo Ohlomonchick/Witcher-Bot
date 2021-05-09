67210b0015ad8be4fd0be41a509a566b9b58b97e
Ohlomonchick <64347118+Ohlomonchick@users.noreply.github.com>
2021-05-09 23:49:44 +0300

    web_started

--- witcher/game/views.py
+++ witcher/game/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from django.views.generic import ListView
+
+from game.models import Profile
 
 
-def index(request):
-    return HttpResponse('Главная')
+class Home(ListView):
+    # paginate_by = 15
+    model = Profile
+    template_name = 'game/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Список лидеров'
+        return context
+
+    def get_queryset(self):
+        return Profile.objects.all()
